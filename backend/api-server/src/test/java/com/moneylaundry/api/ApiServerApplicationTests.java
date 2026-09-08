230516b666f8c5f3a3dc5afdd386a3f6023e3ced
dh0728 <dustkscjswo@naver.com>
2026-09-08 15:28:35 +0900

    MONEY17-77 [fix] postgre 버전수정

--- backend/api-server/src/test/java/com/moneylaundry/api/ApiServerApplicationTests.java
+++ backend/api-server/src/test/java/com/moneylaundry/api/ApiServerApplicationTests.java
@@ -3,19 +3,10 @@ package com.moneylaundry.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.postgresql.PostgreSQLContainer;
 
-@Testcontainers
 @SpringBootTest
 @Import(TestcontainersConfiguration.class)
 class ApiServerApplicationTests {
-
-  @Container
-  @ServiceConnection
-  static final PostgreSQLContainer POSTGRES = new PostgreSQLContainer("postgres:17-alpine");
 
   @Test
   void contextLoads() {}
